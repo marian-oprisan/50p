bd296ca48dd2d91b2f02f38a52aed72df7e86f7f
Marian Oprisan <marian.razvan.oprisan@gmail.com>
2020-12-08 00:07:26 +0200

    Finished Project

--- rotating_navigation/script.js
+++ rotating_navigation/script.js
@@ -2,5 +2,6 @@ const open = document.getElementById('open');
 const close = document.getElementById('close');
 const container = document.querySelector('.container');
 
+// Adding events listeners for open/close
 open.addEventListener('click', () => container.classList.add('show-nav'));
 close.addEventListener('click', () => container.classList.remove('show-nav'));
